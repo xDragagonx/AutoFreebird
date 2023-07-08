503226f0dbe482ab2f3187063ae41e4e86c88195
Dragagon <gilles.depauw@student.vives.be>
2023-07-08 19:07:04 +0200

    updated locales file and changed name to a more fitting name

--- SourceCode.lua
+++ SourceCode.lua
@@ -1,13 +1,5 @@
--- counter keeps adding to targets when mounting || Solved, was using https://alloder.pro/md/LuaApi/FunctionCommonUnRegisterEventHandler.html instead of https://alloder.pro/md/LuaApi/FunctionCommonUnRegisterEvent.html, so it would always register still. Ask pasidaips how it works with the if name == gift of tensess.
--- Make it only work when out of combat
 --Use common.LogInfo("common", "-"..name.."-") --Log to mods.txt
 --Use tostring() to concatenate non-string values in ChatLog()
--- Solved:
---  Error while running the chunk
---   [string "Mods/Addons/RessCounter/Script.luac"]:0: attempt to perform arithmetic on a nil value
---   func: __sub, metamethod, line: -1, defined: C, line: -1, [C]
---     func: ?, ?, line: 0, defined: Lua, line: 0, [string "Mods/Addons/RessCounter/Script.luac"]
-
 --VARIABLES
 local firstSwap = false
 local defaultMaskId
@@ -25,7 +17,7 @@ end
 function EVENT_AVATAR_ZONE_CHANGED()
 	local WanderingActorMaskId = GetWanderingActorMaskId()
 	local freeBirdMaskId = GetFreebirdMaskId()
-	ChatLog("FreebirdmaskId: ", freeBirdMaskId, "WanderingActorMaskId", WanderingActorMaskId)
+	--ChatLog("FreebirdmaskId: ", freeBirdMaskId, "WanderingActorMaskId", WanderingActorMaskId)
 	-- if AreWeInIrene() then
 	-- 	ChatLog("Changed zone, checking if we were already in Irene: ", alreadyInIrene)
 	-- 	if alreadyInIrene == false then
@@ -43,58 +35,58 @@ function EVENT_AVATAR_ZONE_CHANGED()
 	-- 	end
 	-- end
 	if AreWeInTheatre() then
-		ChatLog("Changed subZone, checking if we enter Theatre: ",currentSubZone)
-		--if alreadyInIrene == false then
-			ChatLog("Do you own WanderingActorMask?: ",CostumeIsOwned(WanderingActorMaskId))
-			if CostumeIsOwned(WanderingActorMaskId) then --Check if the player owns the Freebird mask.
-				--common.RegisterEventHandler(EVENT_CHECKROOM_ITEM_CHANGED,"EVENT_CHECKROOM_ITEM_CHANGED") --https://alloder.pro/md/LuaApi/EventCheckroomItemChanged.html
-				OpenWardrobe()
-				checkroomLib.EquipItem( WanderingActorMaskId )
-				ChatLog("equipping WanderingActorMaskId")
-				common.RegisterEventHandler(CloseWardrobeCustom,"EVENT_SECOND_TIMER") --Starts delay before closing the wardrobe.
-				--alreadyInIrene = true
-			else
-				ChatLog("You don't own the Wandering Actor Mask") --Tells the player he doesn't have the Freebird mask.
-			end
-		--end
+	--ChatLog("Changed subZone, checking if we enter Theatre: ",currentSubZone)
+	--if alreadyInIrene == false then
+		--ChatLog("Do you own WanderingActorMask?: ",CostumeIsOwned(WanderingActorMaskId))
+		if CostumeIsOwned(WanderingActorMaskId) then --Check if the player owns the Freebird mask.
+			--common.RegisterEventHandler(EVENT_CHECKROOM_ITEM_CHANGED,"EVENT_CHECKROOM_ITEM_CHANGED") --https://alloder.pro/md/LuaApi/EventCheckroomItemChanged.html
+			OpenWardrobe()
+			checkroomLib.EquipItem( WanderingActorMaskId )
+			--ChatLog("equipping WanderingActorMaskId")
+			common.RegisterEventHandler(CloseWardrobeCustom,"EVENT_SECOND_TIMER") --Starts delay before closing the wardrobe.
+			--alreadyInIrene = true
+		else
+			ChatLog(locales["NoWanderingMask"]) --Tells the player he doesn't have the Freebird mask.
+		end
+	--end
 	elseif AreWeInIrene() then
-		ChatLog("Changed zone, checking if we were already in Irene: ", alreadyInIrene)
-			if alreadyInIrene == false then
-				ChatLog("Do you own FreebirdMaskId?: ",CostumeIsOwned(freeBirdMaskId))
-				if CostumeIsOwned(freeBirdMaskId) then --Check if the player owns the Freebird mask.
-					common.RegisterEventHandler(EVENT_CHECKROOM_ITEM_CHANGED,"EVENT_CHECKROOM_ITEM_CHANGED") --https://alloder.pro/md/LuaApi/EventCheckroomItemChanged.html
-					OpenWardrobe()
-					checkroomLib.EquipItem( freeBirdMaskId )
-					ChatLog("equipping Freebird mask")
-					common.RegisterEventHandler(CloseWardrobeCustom,"EVENT_SECOND_TIMER") --Starts delay before closing the wardrobe.
-					alreadyInIrene = true
-				else
-					ChatLog("You don't own the Freebird Mask") --Tells the player he doesn't have the Freebird mask.
-				end
+		--ChatLog("Changed zone, checking if we were already in Irene: ", alreadyInIrene)
+		if alreadyInIrene == false then
+			--ChatLog("Do you own FreebirdMaskId?: ",CostumeIsOwned(freeBirdMaskId))
+			if CostumeIsOwned(freeBirdMaskId) then --Check if the player owns the Freebird mask.
+				common.RegisterEventHandler(EVENT_CHECKROOM_ITEM_CHANGED,"EVENT_CHECKROOM_ITEM_CHANGED") --https://alloder.pro/md/LuaApi/EventCheckroomItemChanged.html
+				OpenWardrobe()
+				checkroomLib.EquipItem( freeBirdMaskId )
+				--ChatLog("equipping Freebird mask")
+				common.RegisterEventHandler(CloseWardrobeCustom,"EVENT_SECOND_TIMER") --Starts delay before closing the wardrobe.
+				alreadyInIrene = true
+			else
+				ChatLog(locales["NoFreeBirdMask"]) --Tells the player he doesn't have the Freebird mask.
 			end
+		end
 	end
 	--Requipping the previous headwear when leaving Irene.
 	if previousZone == "Airin" and currentZone ~= "Airin" then --Checking if we're leaving Irene.
-		ChatLog("We have left Irene. Previous zone: ",previousZone," and current zone: ", currentZone)
+		--ChatLog("We have left Irene. Previous zone: ",previousZone," and current zone: ", currentZone)
 		OpenWardrobe()
 		checkroomLib.EquipItem( defaultMaskId ) --Requipping the default headwear.
-		ChatLog("equipped default mask")
+		--ChatLog("equipped default mask")
 		common.RegisterEventHandler(CloseWardrobeCustom,"EVENT_SECOND_TIMER") --Starts delay before closing the wardrobe.
 		if firstSwap == true then --resetting flag for saving the default headwear.
 			firstSwap = false
 		end
-		ChatLog("1 defaultmaskid is", defaultMaskId)
+		--ChatLog("1 defaultmaskid is", defaultMaskId)
 		if defaultMaskId ~= nil then --resetting Id of the default headwear.
 			defaultMaskId = nil
-			ChatLog("2 defaultmaskid is", defaultMaskId)
+			--ChatLog("2 defaultmaskid is", defaultMaskId)
 		end
 	end
 	--Requipping the FreeBirdmask headwear when leaving the Theatre.
-	if currentSubZone ~= "Miracle Theatre" and previousSubZone == "Miracle Theatre" then --Checking if we're leaving the Theatre.
-		ChatLog("We have left Theatre. Previous subZone: ",previousSubZone," and current subZone: ", currentSubZone)
+	if currentSubZone ~= locales["TheatreLocation"] and previousSubZone == locales["TheatreLocation"] then --Checking if we're leaving the Theatre.
+		--ChatLog("We have left Theatre. Previous subZone: ",previousSubZone," and current subZone: ", currentSubZone)
 		OpenWardrobe()
 		checkroomLib.EquipItem( freeBirdMaskId ) --Requipping the default headwear.
-		ChatLog("equipped FreeBird mask")
+		--ChatLog("equipped FreeBird mask")
 		common.RegisterEventHandler(CloseWardrobeCustom,"EVENT_SECOND_TIMER") --Starts delay before closing the wardrobe.
 		-- if firstSwap == true then --resetting flag for saving the default headwear.
 		-- 	firstSwap = false
@@ -107,6 +99,7 @@ function EVENT_AVATAR_ZONE_CHANGED()
 	end
 end
 function GetCategories() --https://alloder.pro/md/LuaApi/FunctionCheckroomLibGetCategories.html
+	-- Was used to figure out the addon
 	local categories = checkroomLib.GetCategories()
 	for k, v in pairs(categories) do
 		ChatLog("key: ",k, "value: ",userMods.FromWString(v:GetInfo().name))
@@ -114,13 +107,14 @@ function GetCategories() --https://alloder.pro/md/LuaApi/FunctionCheckroomLibGet
 	--ChatLog( categories[1]:GetInfo().name )
 end
 function GetCollections() --https://alloder.pro/md/LuaApi/FunctionCheckroomLibGetCollections.html
+	-- Was used to figure out the addon
 	local categories = checkroomLib.GetCategories()
 	local collections = checkroomLib.GetCollections( categories[ 3 ] ) --Gets the collections from a specified category
 	--ChatLog(categories[3]:GetInfo())
 	-- for k, v in pairs(collections) do
 	-- 	ChatLog(k, v:GetInfo().name)
 	-- end
-	ChatLog( collections[ 13 ]:GetInfo().name )
+	--ChatLog( collections[ 13 ]:GetInfo().name )
 end
 function GetFreebirdMaskId() --https://alloder.pro/md/LuaApi/FunctionCheckroomLibGetItems.html
 	local itemId
@@ -162,44 +156,43 @@ function CostumeIsOwned(itemId) --https://alloder.pro/md/LuaApi/FunctionCheckroo
 	return isItemInCheckroom
 end
 function OpenWardrobe()
-	ChatLog("Opening the wardrobe")
+	--ChatLog("Opening the wardrobe")
 	if not checkroomLib.IsOpened() then
 		checkroomLib.Open()
 	  end
 end
 function CloseWardrobe()
-	
-	ChatLog("entering func CloseWardrobe()")	
-	ChatLog("Checking if wardrobe is open",checkroomLib.IsOpened())
+	--ChatLog("entering func CloseWardrobe()")	
+	--ChatLog("Checking if wardrobe is open",checkroomLib.IsOpened())
 	if checkroomLib.IsOpened() then
-		ChatLog("Closing the wardrobe")
+		--ChatLog("Closing the wardrobe")
 		checkroomLib.Close()
 	end
 end
 function AreWeInIrene()
-	ChatLog("entering AreWeInIrene() func")
+	--ChatLog("entering AreWeInIrene() func")
 	previousZone = currentZone --Remembers the previous zone.
 	currentZone = GetCurrentZone()
-	ChatLog("We are in: ",currentZone," and came from ", previousZone)
-	if currentZone == "Airin" then --Should be glocal across clients
-		ChatLog("returning true")
+	--ChatLog("We are in: ",currentZone," and came from ", previousZone)
+	if currentZone == "Airin" then --Should be glocal across different language clients
+		--ChatLog("returning true")
 		return true
 	else
-		ChatLog("returning false")
+		--ChatLog("returning false")
 		alreadyInIrene = false
 		return false
 	end
 end
 function AreWeInTheatre()
-	ChatLog("entering AreWeInTheatre() func")
+	--ChatLog("entering AreWeInTheatre() func")
 	previousSubZone = currentSubZone
 	currentSubZone = GetCurrentSubZone()
-	ChatLog("We are in: ",currentSubZone," and came from ", previousSubZone)
-	if currentSubZone == "Miracle Theatre" then --Need locales file
-		ChatLog("returning true")
+	--ChatLog("We are in: ",currentSubZone," and came from ", previousSubZone)
+	if currentSubZone == locales["TheatreLocation"] then --Different result per language
+		--ChatLog("returning true")
 		return true
 	else
-		ChatLog("returning false")
+		--ChatLog("returning false")
 		--alreadyInIrene = false
 		return false
 	end
@@ -213,21 +206,19 @@ function GetCurrentSubZone()
 	return currentMapName
 end
 function EVENT_CHECKROOM_ITEM_CHANGED(params)
-	ChatLog("entering func EVENT_CHECKROOM_ITEM_CHANGED")
-	
-		ChatLog("Already in Irene was: ", alreadyInIrene, "(false) and firstswap is: ", firstSwap,"(false)")
-		if not firstSwap then
-			defaultMaskId = params.itemId
-			ChatLog("Remembering default mask: ",defaultMaskId)
-			firstSwap = true
-		end
-	
+	--ChatLog("entering func EVENT_CHECKROOM_ITEM_CHANGED")
+	--ChatLog("Already in Irene was: ", alreadyInIrene, "(false) and firstswap is: ", firstSwap,"(false)")
+	if not firstSwap then
+		defaultMaskId = params.itemId
+		--ChatLog("Remembering default mask: ",defaultMaskId)
+		firstSwap = true
+	end
 	common.UnRegisterEventHandler(EVENT_CHECKROOM_ITEM_CHANGED,"EVENT_CHECKROOM_ITEM_CHANGED")
 end
 function CloseWardrobeCustom()
-	ChatLog("entering event second timer func & timerCount is: ",timerCount)
+	--ChatLog("entering event second timer func & timerCount is: ",timerCount)
 	if timerCount == 1 then
-		ChatLog("timerCount reached condition")
+		--ChatLog("timerCount reached condition")
 		CloseWardrobe()
 		common.UnRegisterEventHandler(CloseWardrobeCustom,"EVENT_SECOND_TIMER")
 		timerCount = 0
--- locales.lua
+++ locales.lua
@@ -5,30 +5,23 @@ locales["rus"]={}
 locales["fr"]={}
 locales["ger"]={}
 locales["tr"]={}
---Gift of Tensess
-locales["eng_eu"][ "ressName" ] = "Gift of Tensess"
-locales["fr"][ "ressName" ] = "Don de Tensess"
-locales["rus"][ "ressName" ] = "Gift of Tensess"
-locales["ger"][ "ressName" ] = "Geschenk von Tensess"
-locales["tr"][ "ressName" ] = "Tenses’in H/252neri"
---ChatLog output sum of resses
-locales["eng_eu"][ "chatLogMid" ] = "has been ressed"
-locales["fr"][ "chatLogMid" ] = "a ete ressuscite"
-locales["rus"][ "chatLogMid" ] = "возрождался"
-locales["ger"][ "chatLogMid" ] = "wurde"
-locales["tr"][ "chatLogMid" ] = ""
-
-locales["eng_eu"][ "chatLogEnd" ] = "times."
-locales["fr"][ "chatLogEnd" ] = "fois."
-locales["rus"][ "chatLogEnd" ] = "раз."
-locales["ger"][ "chatLogEnd" ] = "wiederbelebt."
-locales["tr"][ "chatLogEnd" ] = "kez dirildi."
-
-locales["eng_eu"][ "noData" ] = "no data."
-locales["fr"][ "noData" ] = "Pas de donn/233es."
-locales["rus"][ "noData" ] = "Нет данных."
-locales["ger"][ "noData" ] = "Keine Daten."
-locales["tr"][ "noData" ] = "Veri yok."
-
+--Player doesn't own Wandering Mask
+locales["eng_eu"][ "NoWanderingMask" ] = "You don't own the Wandering Actor Mask"
+locales["fr"][ "NoWanderingMask" ] = "Vous ne poss/233dez pas le masque d'acteur errant"
+locales["rus"][ "NoWanderingMask" ] = "У вас нет маски странствующего актера"
+locales["ger"][ "NoWanderingMask" ] = "Sie besitzen die Wandering Actor-Maske nicht"
+locales["tr"][ "NoWanderingMask" ] = "Wandering Actor Mask'e sahip de/287ilsiniz"
+--Player doesn't own Freebird Mask
+locales["eng_eu"][ "NoFreeBirdMask" ] = "You don't own the Free Bird Mask"
+locales["fr"][ "NoFreeBirdMask" ] = "Vous ne poss/233dez pas le masque d'oiseau gratuit"
+locales["rus"][ "NoFreeBirdMask" ] = "У вас нет маски Free Bird."
+locales["ger"][ "NoFreeBirdMask" ] = "Sie besitzen die Free Bird Mask nicht"
+locales["tr"][ "NoFreeBirdMask" ] = "Free Bird Mask'e sahip de/287ilsiniz"
+--Theatre location
+locales["eng_eu"][ "TheatreLocation" ] = "Miracle Theatre"
+locales["fr"][ "TheatreLocation" ] = "Th/233/226tre Miracle"
+locales["rus"][ "TheatreLocation" ] = "Чудо-театр"
+locales["ger"][ "TheatreLocation" ] = "Wundertheater"
+locales["tr"][ "TheatreLocation" ] = "Mucize Tiyatrosu"
 
 locales = locales[common.GetLocalization()] -- trims all other languages except the one that common.getlocal got.
